862107ee99bcc1741b84d10ddd42180cac462ec6
helux <kled.master@gmail.com>
2024-07-02 03:23:20 +0600

    Создана новая колонна details и модернизированы модели с шаблонами под неё

--- webapp/models.py
+++ webapp/models.py
@@ -11,6 +11,7 @@ class Task(models.Model):
     description = models.CharField(max_length=255)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     due_date = models.DateField(blank=True, null=True)
+    details = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.description
