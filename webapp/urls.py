a94747453458aa28f9bd4f2c75725c7e39bc92e0
helux <kled.master@gmail.com>
2024-07-08 20:10:52 +0600

    CRUD завершен

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
-    path('product/<int:pk>/', views.detail_task, name='detail_task'),
+    path('task/<int:pk>/', views.detail_task, name='detail_task'),
+    path('task/<int:pk>/update/', views.update_task, name='update_task'),
+    path('task/<int:pk>/delete/', views.delete_task, name='delete_task'),
 ]
+
--- webapp/views.py
+++ webapp/views.py
@@ -22,3 +22,36 @@ def add_task(request):
 def detail_task(request, pk, **kwargs):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'detail_task.html', {'task': task})
+def update_task(request, *args, pk, **kwargs):
+    if request.method == "GET":
+        task = get_object_or_404(Task, pk=pk)
+        form = TaskForm(initial={
+            "description": task.description,
+            "due_date": task.due_date,
+            "status": task.status,
+        })
+        return render(
+            request, "update_task.html",
+            context={"form": form}
+        )
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = get_object_or_404(Task, pk=pk)
+            task.description = request.POST.get("description")
+            task.due_date = request.POST.get("due_date")
+            task.status = request.POST.get("status")
+            task.save()
+            return redirect("task_list")
+        else:
+            return render(
+                request,
+                "update_task.html",
+                {"form": form}
+            )
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'delete_task.html', {'task': task})
