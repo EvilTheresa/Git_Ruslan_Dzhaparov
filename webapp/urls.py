97b1ca430a33106cdd1ff795151fd3a1c94aa1e4
helux <kled.master@gmail.com>
2024-07-08 19:50:45 +0600

    Детальный просмотр и шаблоны

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
+    path('product/<int:pk>/', views.detail_task, name='detail_task'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
 
 def task_list(request):
     tasks = Task.objects.all()
-    print("hello")
     return render(request, 'home.html', {'tasks': tasks})
 
 
@@ -18,3 +17,8 @@ def add_task(request):
     else:
         form = TaskForm()
     return render(request, 'add_task.html', {'form': form})
+
+
+def detail_task(request, pk, **kwargs):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'detail_task.html', {'task': task})
